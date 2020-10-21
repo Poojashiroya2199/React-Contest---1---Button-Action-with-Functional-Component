406e9c2b99ea237ed558b9506164325ef636bc8c
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-10-21 21:07:31 +0530

    change3

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let para = false;
   const handleClick = () => {
     return (
       <p id="para">
@@ -14,8 +15,8 @@ function App() {
     <>
       <div id="main">
         {/*Do not alter the main div*/}
-        <button id="click" onClick={handleClick}>
-          hello
+        <button id="click" onClick={(para = true)}>
+          {para ? handleClick : ""}
         </button>
       </div>
     </>
