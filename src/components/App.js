642a102c0d34833d4ff6761148c099f0a5511ab1
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-10-21 20:53:26 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const handleClick = () => {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  };
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+    <>
+      <div id="main">// Do not alter the main div</div>
+      <button id="click" onClick={handleClick}>
+        hello
+      </button>
+    </>
   );
 }
-
 
 export default App;
