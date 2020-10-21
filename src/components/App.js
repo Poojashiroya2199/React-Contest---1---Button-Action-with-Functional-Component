86c517ef52d27c403c9f63592c2f9cf3096bd2ad
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-10-21 21:11:55 +0530

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,19 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let para = false;
-  const handleClick = () => {
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
-  };
   return (
     <>
       <div id="main">
         {/*Do not alter the main div*/}
-        <button id="click" onClick={(para = true)}>
-          {para ? handleClick : ""}
-        </button>
+        <button
+          id="click"
+          onClick={() => (
+            <p id="para">
+              Hello, I've learnt to use the full-stack evaluation tool. This
+              makes me so happy
+            </p>
+          )}
+        ></button>
       </div>
     </>
   );
