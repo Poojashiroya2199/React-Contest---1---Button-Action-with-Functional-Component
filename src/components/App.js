ea69fec0733fb12fdea1b78c702345de6cf856b5
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-11-07 16:24:27 +0530

    x

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [chek, setchek] = useState(false);
-
-  const handleclick = () => {
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
-  };
+  let [chek, setchek] = useState(true);
+  // function para() {
+  //   return (
+  //     <p id="para">
+  //       Hello, I've learnt to use the full-stack evaluation tool. This makes me
+  //       so happy
+  //     </p>
+  //   );
+  // }
   return (
     <>
-      <div id="main">
-        {/*Do not alter the main div*/}
-        <button id="click" onClick={setchek(true)}>
-          {chek ? { handleclick } : ""}
-        </button>
-      </div>
+      <div id="main">Do not alter the main div</div>
+      <button id="click" onClick={setchek(false)}>
+        {chek ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : (
+          ""
+        )}
+      </button>
     </>
   );
 }
